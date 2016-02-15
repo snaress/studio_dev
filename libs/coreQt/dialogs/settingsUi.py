d9c9ac04f6786cba166f997465b6fcfc71c5b5e6
julien <jln.buisseret@gmail.com>
2016-02-15 02:28:23 +0100

    libs.coreQt.dialogs: Fix settings compileUi

--- libs/coreQt/dialogs/settingsUi.py
+++ libs/coreQt/dialogs/settingsUi.py
@@ -1,9 +1,10 @@
 import os
-from coreQt import pQt
 from PyQt4 import QtGui
 from coreSys import env
+from coreQt import pQt, dialogs
 
 
+dialogs.compileUi('dial_settings.ui')
 from _ui import dial_settingsUI
 class Settings(QtGui.QDialog, dial_settingsUI.Ui_dial_settings):
     """
